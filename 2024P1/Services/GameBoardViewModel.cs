476b5fc054d3e5d586788507443bc63e15658cb5
dylanwhitehurst <dylan.whitehurst@nordson.com>
2024-12-09 11:41:36 -0700

    updated card place to be a object instead of a string

--- 2024P1/Services/GameBoardViewModel.cs
+++ 2024P1/Services/GameBoardViewModel.cs
@@ -15,7 +15,7 @@ public interface IGameBoardViewModel
     bool CanCardBeMovedToBoardPos(CardVM card, (int x, int y) destPos);
     bool CanCardBeMovedToHand(CardVM card);
     
-    void MoveCard(CardVM cardVm, string moveToPlace);
+    void MoveCard(CardVM cardVm, CardPlace moveToPlace);
     void PlayCards();
 
 }
@@ -37,7 +37,7 @@ public class GameBoardViewModel : IGameBoardViewModel
         _wordValidator = wordValidator;
 
         foreach (Card card in handService.Cards)
-            _cardVmDict.Add(card.Id, new CardVM(card, "hand"));
+            _cardVmDict.Add(card.Id, new CardVM(card, new CardPlace() { IsHand = true }));
     }
 
     private Dictionary<int, CardVM> _cardVmDict = new();
@@ -56,44 +56,42 @@ public class GameBoardViewModel : IGameBoardViewModel
 
     public bool CanCardBeMovedToBoardPos(CardVM card, (int x, int y) destPos)
     {
-        if (card.Place == "hand")
+        if (card.Place.IsHand)
             return true;
         
-        var cardPos = GetBoardPosition(card.Place);
         // a single card should not be able to be moved adjacent to itself
         // even if this wouldn't technically break the board
         if (_boardService.Cards.Count == 1)
             return false;
         
-        return _boardService.WouldCardBreakBoardIfMoved(cardPos, destPos);
+        return _boardService.WouldCardBreakBoardIfMoved(card.Place.BoardPos.Value, destPos);
     }
     
     public bool CanCardBeMovedToHand(CardVM card)
     {
-        if (card.Place == "hand")
+        if (card.Place.IsHand)
             return true;
         
-        var cardPos = GetBoardPosition(card.Place);
+        var cardPos = card.Place.BoardPos.Value;
         // a single card can move to hand
         if (_boardService.Cards.Count == 1)
             return true;
         return _boardService.WouldCardBreakBoardIfGone(cardPos);
     }
 
-    public void MoveCard(CardVM cardVm, string moveToPlace)
+    public void MoveCard(CardVM cardVm, CardPlace moveToPlace)
     {
         // remove card from old place
         Card removedCard;
-        if (cardVm.Place == "hand")
+        if (cardVm.Place.IsHand)
             removedCard = _handService.Remove(cardVm.Id);
         else
-            removedCard = _boardService.Remove(GetBoardPosition(cardVm.Place));
+            removedCard = _boardService.Remove(cardVm.Place.BoardPos.Value);
 
         // add card to new place
-        if (moveToPlace != "hand")
+        if (!moveToPlace.IsHand)
         {
-            var boardPos = GetBoardPosition(moveToPlace);
-            _boardService.Add(removedCard, boardPos);
+            _boardService.Add(removedCard, moveToPlace.BoardPos.Value);
         }
         else
             _handService.Add(removedCard);
@@ -101,16 +99,10 @@ public class GameBoardViewModel : IGameBoardViewModel
         // synchronize change that happened with the board with the model
         _cardVmDict[cardVm.Id].Place = moveToPlace;
     }
-    private static (int x, int y) GetBoardPosition(string place)
-    {
-        var match = Regex.Match(place, @"\((-?\d+),(-?\d+)\)");
-        var pos = (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
-        return pos;
-    }
     
     public void PlayCards()
     {
-        foreach (var card in Cards.Where(c => c.Place != "hand"))
+        foreach (var card in Cards.Where(c => !c.Place.IsHand))
         {
             card.Highlight = CardHighlight.Success;
         }
@@ -125,7 +117,7 @@ public class CardVM
     {
         
     }
-    public CardVM(Card card, string place)
+    public CardVM(Card card, CardPlace place)
     {
         if (card is LetterCard letterCard)
         {
@@ -143,8 +135,39 @@ public class CardVM
     public int Id { get; set; }
     public string Name { get; set; }
     public string Text { get; set; }
-    public string Place { get; set; }
+    public CardPlace Place { get; set; }
     public CardHighlight Highlight { get; set; } = CardHighlight.None;
+}
+
+public class CardPlace
+{
+    public bool IsHand { get; set; }
+    public (int x, int y)? BoardPos { get; set; }
+    
+    public CardPlace() {}
+
+    public CardPlace(string dropzoneRep)
+    {
+        if (dropzoneRep == "hand")
+            IsHand = true;
+        else
+        {
+            IsHand = false;
+            var match = Regex.Match(dropzoneRep, @"^board-\((-?\d+),(-?\d+)\)$");
+            var pos = (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value));
+            BoardPos = pos;
+        }
+    }
+
+    public override string ToString()
+    {
+        if (IsHand)
+            return "hand";
+        else
+            return $"board-({BoardPos.Value.x},{BoardPos.Value.y})";
+    }
+
+    public static CardPlace InHand => new CardPlace() { IsHand = true };
 }
 
 public enum CardHighlight
--- 2024P1/Services/GameService.cs
+++ 2024P1/Services/GameService.cs
@@ -12,9 +12,9 @@ public class StubGameService : IGameService
     public List<CardVM> DrawHand()
     {
          return [
-            new CardVM() { Id = 1, Name = "Card a", Text = "a", Place = "Hand" },
-            new CardVM() { Id = 2, Name = "Card b", Text = "b", Place = "Hand" },
-            new CardVM() { Id = 3, Name = "Card c", Text = "c", Place = "Hand" }
+            new CardVM() { Id = 1, Name = "Card a", Text = "a", Place = CardPlace.InHand },
+            new CardVM() { Id = 2, Name = "Card b", Text = "b", Place = CardPlace.InHand },
+            new CardVM() { Id = 3, Name = "Card c", Text = "c", Place = CardPlace.InHand }
         ];
     }
 }
